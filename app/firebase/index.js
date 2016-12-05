6c259f4ae829298d46a275f5aafa5b1072f2648f
birdcheng <birdchengwww@gmail.com>
2016-12-05 19:46:46 +0800

    move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyC0ezB_sk3PhCTc-0XXQkKNfzsWAeoh2B8",
-        authDomain: "mead-todo-app-320bf.firebaseapp.com",
-        databaseURL: "https://mead-todo-app-320bf.firebaseio.com",
-        storageBucket: "mead-todo-app-320bf.appspot.com",
-        messagingSenderId: "441261971677"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
       };
       firebase.initializeApp(config);
 } catch (e) {
